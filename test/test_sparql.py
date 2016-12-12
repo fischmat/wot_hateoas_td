5627b77ffc16c873c89a84999d865ba5cc7082a8
Matthias Fisch <mail@matthias-fisch.de>
2016-12-12 12:35:02 +0100

    All tests passing

--- test/test_sparql.py
+++ test/test_sparql.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from src import sparql
+from src.sparql import SPARQLNamespaceRepository, UnknownPrefixException
 
 
 class Test_Sparql(TestCase):
@@ -14,3 +15,14 @@ class Test_Sparql(TestCase):
     def test_classes_equivalent(self):
         self.assertTrue(sparql.classes_equivalent('http://purl.org/iot/vocab/m3-lite#Thermometer', 'http://purl.oclc.org/NET/ssnx/meteo/aws#TemperatureSensor'))
         self.assertFalse(sparql.classes_equivalent('http://purl.org/iot/vocab/m3-lite#Thermometer', 'http://elite.polito.it/ontologies/dogont.owl#DeepFreezer'))
+
+    def test_namespace_repository(self):
+        ns = SPARQLNamespaceRepository()
+        ns.register('dogont', 'http://elite.polito.it/ontologies/dogont.owl#')
+        self.assertEqual(ns.resolve('dogont:Lighting'), 'http://elite.polito.it/ontologies/dogont.owl#Lighting')
+
+        with self.assertRaises(UnknownPrefixException):
+            ns.resolve('unknownont:Lighting')
+
+        with self.assertRaises(ValueError):
+            ns.resolve('nonshorthandgibberish')
