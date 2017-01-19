4b1cbf86151497fd605733a10079c53a687606ac
Matthias Fisch <mail@matthias-fisch.de>
2017-01-19 12:41:25 +0100

    Added documentation. Minor bugfix.

--- src/doorsensor/doorsensor_td.py
+++ src/doorsensor/doorsensor_td.py
@@ -14,23 +14,29 @@ GPIO_ECHO = 24
 PORT_NUMBER = 8080
 BASE_URL = 'http://192.168.42.100:%d' % PORT_NUMBER
 
+# Tolerance of distance measurements in cm:
 DISTANCE_EPSILON = 1.0
+# Time in seconds after which measured distance values are considered correct:
 DISTANCE_CALIBRATION_TIME = 5
 
+# Unix-timestamp of the last recognized door opening:
 last_door_open_time = 0
 
-# import RPi.GPIO as GPIO
-# # GPIO Mode (BOARD / BCM)
-# GPIO.setmode(GPIO.BCM)
-#
-# # set GPIO direction (IN / OUT)
-# GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-# GPIO.setup(GPIO_ECHO, GPIO.IN)
+import RPi.GPIO as GPIO
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 
 def observe_door_status():
-    start_time = datetime.now()
+    # Log the timestamp when starting to measure. Marks begin of calibration time.
+    start_time = time.time()
+    # The distance in cm measured during the last cycle:
     last_distance = 0
+
     while True:
         # set Trigger to HIGH
         GPIO.output(GPIO_TRIGGER, True)
@@ -39,34 +45,40 @@ def observe_door_status():
         time.sleep(0.00001)
         GPIO.output(GPIO_TRIGGER, False)
 
-        StartTime = time.time()
-        StopTime = time.time()
+        start_time = time.time()
+        stop_time = time.time()
 
         # save StartTime
         while GPIO.input(GPIO_ECHO) == 0:
-            StartTime = time.time()
+            start_time = time.time()
 
         # save time of arrival
         while GPIO.input(GPIO_ECHO) == 1:
-            StopTime = time.time()
+            stop_time = time.time()
 
         # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
+        time_elapsed = stop_time - start_time
         # multiply with the sonic speed (34300 cm/s)
         # and divide by 2, because there and back
-        distance = (TimeElapsed * 34300) / 2
+        distance = (time_elapsed * 34300) / 2
 
-        # Measured distance is lower than the last measured and calibration interval is over.
-        now = datetime.now()
-        if distance < last_distance - DISTANCE_EPSILON and (now - start_time).total_seconds() > DISTANCE_CALIBRATION_TIME:
-            last_door_open_time = now.timestamp()
+        # Check whether this is still in the calibration interval:
+        now = time.time()
+        in_calibration = now - start_time < DISTANCE_CALIBRATION_TIME
 
+        # If distance is diminished since last measurement, assume the door opened.
+        # Omit setting the door open time during calibration phase, so no false positives occure
+        if not in_calibration and distance < last_distance - DISTANCE_EPSILON:
+            global last_door_open_time
+            last_door_open_time = now
+
+        # Log the distance measured in this cycle:
         last_distance = distance
+        # Wait some time until next measurement
         time.sleep(0.5)
 
 
-# This class will handles any incoming request from
-# the browser
+# Handles HTTP requests done
 class DoorTDRequestHandler(BaseHTTPRequestHandler):
 
     open_event_signalized = {}
@@ -74,6 +86,7 @@ class DoorTDRequestHandler(BaseHTTPRequestHandler):
     # Handler for the GET requests
     def do_GET(self):
         if not self.path or self.path == "/":
+            # Send the thing description:
             self.send_response(200)
             self.send_header('Content-Type', 'application/thing-description+json')
             self.end_headers()
@@ -126,29 +139,49 @@ class DoorTDRequestHandler(BaseHTTPRequestHandler):
             }).encode())
 
         elif self.path in self.open_event_signalized.keys():
+            # Check whether the last report of this resource was sent after the last door open event,
+            # thus the client already knows about it:
             if self.open_event_signalized[self.path] > last_door_open_time:
-                # Nothing has changed. If there were changes, these have been communicated to the client:
-                self.send_response_only(204, "No Content")
+                self.send_response_only(208) # Send Already Reported
 
             else:
+                # The last door open event is younger than the last report made via this resource.
+                # Report the event to the client.
                 self.send_response(200)
                 self.send_header('Content-Type', 'application/json')
                 self.end_headers()
                 self.wfile.write(json.dumps({
-                    'value': datetime.now().timestamp()
+                    'value': datetime.now().timestamp() # We provide the UNIX-timestamp of the event as data.
                 }))
-                self.open_event_signalized[self.path] = datetime.now().timestamp()
+                # Remember that we have signalized door openings at this point of time:
+                self.open_event_signalized[self.path] = time.time()
+
+        elif self.path == '/openevent': # Only POST is allowed on this resource
+            self.send_response_only(405) # Send Method Not Allowed
+
+        else:
+            self.send_response_only(404) # Send Not Found
 
     def do_POST(self):
-        if self.path == "/openevent":
+        if self.path == "/openevent": # Client wants a new subscription to door open events:
+            # Create a new resource. No configuration data is supported for this event:
             event_resource_uri = '/open_evt_%d' % len(self.open_event_signalized)
-            self.open_event_signalized[event_resource_uri] = datetime.now().timestamp()
+            # Any door opening happened until should not be reported to the client.
+            # So set its last report timestamp to now:
+            self.open_event_signalized[event_resource_uri] = time.timestamp()
 
+            # Send a redirect to the new resource:
             self.send_response(308)
             self.send_header('Location', BASE_URL + event_resource_uri)
             self.end_headers()
 
+        elif not self.path or self.path == '/' or self.path in self.open_event_signalized.keys():
+            self.send_response_only(405) # Send Method Not Allowed
+        else:
+            self.send_response_only(404) # Send Not Found
 
+
+# Start daemon thread montoring the time of the last door opening:
 door_sensor_thread = threading.Thread(target=observe_door_status, args=())
 door_sensor_thread.daemon = True
 door_sensor_thread.start()
