5acbfb6239dcba8633e9f796f84683b54fba0f85
Matthias Fisch <mail@matthias-fisch.de>
2017-01-26 18:31:55 +0100

    Dispatcher for Speaker -> Light in HATEOAS. Missing failure detection.

--- src/dispatcher/__init__.py
+++ src/dispatcher/__init__.py
@@ -1,113 +1,219 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from operator import itemgetter
 from urllib.parse import urlparse
 import http.client as httplib
 
-PORT_NUMBER = 5678
+class NotFoundException(Exception):
+    """
+    Raised when a resource is not found.
+    """
+    pass
 
-class MediaTypeTranslator(object):
+class MethodNotAllowedException(Exception):
+    """
+    Raised when the method of a request is not allowed.
+    """
+    pass
 
-    def input_type(self):
-        raise NotImplementedError("Call to abstract method input_type(). Instances must implement this method!")
+class UnsupportedMediaTypeException(Exception):
+    """
+    Raised when an unsupported media type is provided.
+    """
+    pass
 
-    def output_type(self):
-        raise NotImplementedError("Call to abstract method output_type(). Instances must implement this method!")
+class VirtualMapperResource(object):
+    """
+    Abstract base class for mapping media types.
+    """
+    _mapped_url = None
+    _accept_media_type = None
 
-    def translate(self, input, method, body):
+    def __init__(self, response_media_type, mapped_url):
         """
-        Takes an input of representation described by the mediatype returned by input_type() and translates it to one
-        described by output_type().
-        :param input: The input of appropriate representation.
-        :param method The HTTP method used for accessing the input.
-        :return: The translated input in output representation of this translator.
+        :param response_media_type The media type of '/'
+        :param mapped_url The URL of the thing being mapped.
         """
-        # TODO Use real calling to URLs here!
-        raise NotImplementedError("Call to abstract method translate(). Instances must implement this method!")
+        self._response_media_type = response_media_type
+        self._mapped_url = mapped_url
 
-class HATEOASDispatchService(object):
-
-    _mapped_urls = []
-
-    # Translators indexed by string 'input-mediatype -> output-mediatype'
-    _translators = {}
-
-    def register_url(self, url):
-        """
-        Adds an URL to the list of dispatched resources.
-        Note that currently only HTTP is supported.
-        :param url: The url to add.
-        """
-        parse = urlparse(url)
-        if parse.scheme == 'http': # Only HTTP is allowed
-            self._mapped_urls.append(url)
+    def _fetch_resource(self, url, method="GET", body=None):
+        if body and not isinstance(body, str):
+            seralized_body = json.dumps(body)
+            headers = {'Content-Type': 'application/json'}
+        elif isinstance(body, str):
+            seralized_body = body
+            headers = {'Content-Type': 'text/plain'}
         else:
-            raise ValueError("Only HTTP is supported. Resource with access scheme %s given." % parse.scheme)
+            serialized_body = None
+            headers = {}
 
-    def register_translator(self, t):
+        url_parsed = urlparse(url)
+        conn = httplib.HTTPConnection(url_parsed.netloc)
+        conn.request(method, url_parsed.path, body=serialized_body, headers=headers)
+        response = conn.getresponse()
+
+        if response.code == 200:
+            # Decode JSON responses. Also treat as JSON if no media type specified:
+            if 'Content-Type' not in response.headers or response.headers['Content-Type'].endswith('json'):
+                return json.loads(response.read().decode('utf-8'))
+            else:
+                raise ValueError(
+                    'The resource %s is encoded in unknown media type %s' % (url, response.headers['Content-Type']))
+        else:
+            raise Exception("Received %d %s requesting %s" % (response.code, response.status, url))
+
+    def handle_GET(self, path, headers):
+        raise NotImplementedError("Call to abstract method handle_GET(). Instances must implement this method!")
+
+    def handle_POST(self, path, data, headers):
+        raise NotImplementedError("Call to abstract method handle_POST(). Instances must implement this method!")
+
+    def accept_media_type(self, href):
+        raise NotImplementedError("Call to abstract method accept_media_type(). Instances must implement this method!")
+
+    def mapped_url(self):
+        return self._mapped_url
+
+    def response_media_type(self):
+        return self._response_media_type
+
+
+class HATEOASDispatcherService(object):
+
+    _mappers = {}
+
+    _priorities = {}
+
+    def _next_vresource_href(self):
+        return "/vr_%d" % len(self._mappers)
+
+    def register_mapper_resource(self, r, priority=0):
         """
-        Adds an translator to the dispatcher. If there is already a translator registered for the input/output mediatype
+        Adds a virtual mapper resource to the dispatcher.
+        If there is already a resource registered for the input mediatype/url
         combination then it will be overwritten.
-        :param t: The translator to add. Must implement MediaTypeTranslator
+        :param r: The resource to add. Must implement VirtualMapperResource
+        :param priority: The priority of this virtual resource. If there is another resource with higher priority
+        accepting the same media type, then this resource will not be visible in the bulletin board.
         """
-        if isinstance(t, MediaTypeTranslator):
-            key = "%s -> %s" % (t.input_type(), t.output_type())
+        if isinstance(r, VirtualMapperResource):
+            vr_href = self._next_vresource_href()
+            self._mappers[vr_href] = r
+            self._priorities[vr_href] = priority
         else:
-            raise ValueError("Translators must implement MediaTypeTranslator interface!")
+            raise ValueError("Mappers must implement VirtualMapperResource")
 
-    def _get_request_handler(self, virtual_resources):
+    def _get_request_handler(self, base_url, virtual_resources, priorities):
         class HATEOASDispatchServiceHandler(BaseHTTPRequestHandler):
             _virtual_resources = {}
+            _base_url = ''
+            _priorities = {}
 
             def __init__(self, *args, **kwargs):
-                super(HATEOASDispatchServiceHandler, self).__init__(*args, **kwargs)
                 self._virtual_resources = virtual_resources
-
-            def __fetch_resource(self, url, method="GET", body=None):
-                if body:
-                    seralized_body = json.dumps(body)
-                    headers = {'Content-Type': 'application/json'}
-                else:
-                    serialized_body = None
-                    headers = None
-
-                url_parsed = urlparse(url)
-                conn = httplib.HTTPConnection(url_parsed.netloc)
-                conn.request(method, url_parsed.path, body=serialized_body, headers=headers)
-                response = conn.getresponse()
-
-                if response.code == 200:
-                    # Decode JSON responses. Also treat as JSON if no media type specified:
-                    if 'Content-Type' in response.headers or response.headers['Content-Type'] == 'application/json':
-                        return json.loads(response.read().decode('utf-8'))
-                    else:
-                        raise ValueError('The resource %s is encoded in unknown media type %s' % (url, response.headers['Content-Type']))
-                else:
-                    raise Exception("Received %d %s requesting %s" % (response.code, response.status, url))
+                self._base_url = base_url
+                self._priorities = priorities
+                super(HATEOASDispatchServiceHandler, self).__init__(*args, **kwargs)
 
             def do_GET(self):
+                path_splits = self.path.split('/')
+
                 if self.path == '/':
-                    # TODO Send bulletin board
-                    pass
+                    self.send_response(200)
+                    self.send_header('Content-Type', 'application/bulletin-board+json')
+                    self.end_headers()
 
-                elif self.path in self._virtual_resources.keys():
-                    translator, target_url = self._virtual_resources[self.path]
-                    target_resource = self.__fetch_resource(target_url, method='GET')
+                    items = []
+                    priorizations = []
+                    for vr_path, vr in self._virtual_resources.items():
+                        priorizations.append((vr_path, vr, self._priorities[vr_path]))
+                    priorizations = sorted(priorizations, key=itemgetter(2), reverse=True)
 
+                    seen_media_types = []
+                    priorized_vrs = []
+                    for vr_path, vr, vr_prio in priorizations:
+                        media_type = vr.response_media_type()
+                        if media_type not in seen_media_types:
+                            priorized_vrs.append((vr_path, vr))
+                            seen_media_types.append(media_type)
+
+                    for vr_path, vr in priorized_vrs:
+                        base_url = self._base_url if self._base_url[-1] != '/' else self._base_url[:-1]
+                        try:
+                            item = json.loads(vr.handle_GET('/', {}))
+                            item['_base'] = base_url + vr_path
+                            items.append(item)
+                        except Exception as e:
+                            print(e)
+                            pass
+
+                    self.wfile.write(bytes(json.dumps({
+                        '_embedded': items
+                    }), 'UTF-8'))
+
+                elif len(path_splits) >= 2 and '/' + path_splits[1] in self._virtual_resources.keys():
+                    mapper = self._virtual_resources['/' + path_splits[1]]
+                    try:
+                        href = '/'.join(path_splits[2:]) if len(path_splits) >= 3 else '/'
+                        response = mapper.handle_GET(href, self.headers)
+                    except NotFoundException:
+                        self.send_response_only(404)
+                        return
+                    except UnsupportedMediaTypeException:
+                        self.send_response_only(415)
+                        return
+                    except MethodNotAllowedException:
+                        self.send_response_only(405)
+                        return
+                    except Exception:
+                        self.send_response_only(500)
+                        return
+
+                    self.send_response(200)
+                    self.send_header('Content-Type', mapper.response_media_type())
+                    self.end_headers()
+
+                    self.wfile.write(bytes(response, 'UTF-8'))
+
+                else:
+                    self.send_response_only(404)
+
+            def do_POST(self):
+                path_splits = self.path.split('/')
+
+                if len(path_splits) >= 2 and '/' + path_splits[1] in self._virtual_resources.keys():
+                    mapper = self._virtual_resources['/' + path_splits[1]]
+                    href = '/' + '/'.join(path_splits[2:]) if len(path_splits) >= 3 else '/'
+                    if self.headers['Content-Type'] == mapper.accept_media_type(href):
+                        data = self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8')
+                        try:
+                            response = mapper.handle_POST(href, data, self.headers)
+                        except Exception:
+                            self.send_response_only(500)
+                        self.send_response(200)
+                        self.send_header('Content-Type', 'application/json')
+                        self.end_headers()
+
+                        self.wfile.write(bytes(response, 'UTF-8'))
+                    else:
+                        self.send_response_only(415) # Send Unsupported Media Type
 
 
         return HATEOASDispatchServiceHandler
 
-try:
-    # Create a web server and define the handler to manage the
-    # incoming request
-    server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print
-    'Started httpserver on port ', PORT_NUMBER
+    def start(self, base_url, port=8080):
 
-    # Wait forever for incoming htto requests
-    server.serve_forever()
+        try:
+            # Create a web server and define the handler to manage the
+            # incoming request
+            handler = self._get_request_handler(base_url, self._mappers, self._priorities)
+            server = HTTPServer(('', port), handler)
+            print('Started httpserver on port ', port)
 
-except KeyboardInterrupt:
-    print
-    '^C received, shutting down the web server'
-    server.socket.close()
+            # Wait forever for incoming htto requests
+            server.serve_forever()
+
+        except KeyboardInterrupt:
+            print('^C received, shutting down the web server')
+            server.socket.close()
---
+++ src/dispatcher/dispatcher.py
@@ -0,0 +1,91 @@
+import json
+
+from src.dispatcher import HATEOASDispatcherService, VirtualMapperResource, NotFoundException, \
+    UnsupportedMediaTypeException
+
+class LightAlarmMapper(VirtualMapperResource):
+
+    def __init__(self, mapped_url):
+        super().__init__('application/alarm+json', mapped_url)
+
+    def handle_GET(self, path, headers):
+        if path == '/':
+            light = self._fetch_resource(self.mapped_url(), method='GET')
+            response = json.dumps({
+                '_forms': {
+                    'alarm': {
+                        'href': '/alarm',
+                        'method': 'POST',
+                        'accept': 'application/alarm-invocation+json'
+                    }
+                }
+            })
+            for k, v in light.items():
+                if not k.startswith('_'):
+                    response['k'] = v
+            return json.dumps(response)
+        else:
+            raise NotFoundException("%s does not exist" % path)
+
+    def handle_POST(self, path, data, headers):
+        if path == '/alarm':
+            light = self._fetch_resource(self.mapped_url(), method='GET')
+            if light['_forms']['strobeon']['accept'] == 'application/light-strobe-config+json':
+                self._fetch_resource(url=self.mapped_url() + light['_forms']['strobeon']['href'],
+                                     method= light['_forms']['strobeon']['method'],
+                                     body={
+                                         'duration': 60
+                                     })
+            else:
+                raise UnsupportedMediaTypeException()
+
+    def accept_media_type(self, href):
+        if href == '/alarm':
+            return 'application/alarm-invocation+json'
+        else:
+            return False
+
+class SpeakerAlarmMapper(VirtualMapperResource):
+
+    def __init__(self, mapped_url):
+        super().__init__('application/alarm+json', mapped_url)
+
+    def handle_GET(self, path, headers):
+        if path == '/':
+            speaker = self._fetch_resource(self.mapped_url(), method='GET')
+            response = {
+                '_forms': {
+                    'alarm': {
+                        'href': '/alarm',
+                        'method': 'POST',
+                        'accept': 'application/alarm-invocation+json'
+                    }
+                }
+            }
+            for k, v in speaker.items():
+                if not k.startswith('_'):
+                    response[k] = v
+            return json.dumps(response)
+        else:
+            raise NotFoundException("%s does not exist" % path)
+
+    def handle_POST(self, path, data, headers):
+        if path == '/alarm':
+            speaker = self._fetch_resource(self.mapped_url(), method='GET')
+            if speaker['_forms']['play_alarm']['accept'] == 'text/plain':
+                self._fetch_resource(url=self.mapped_url() + speaker['_forms']['play_alarm']['href'],
+                                     method= speaker['_forms']['play_alarm']['method'])
+            else:
+                raise UnsupportedMediaTypeException()
+
+    def accept_media_type(self, href):
+        if href == '/alarm':
+            return 'application/alarm-invocation+json'
+        else:
+            return False
+
+
+dispatcher = HATEOASDispatcherService()
+dispatcher.register_mapper_resource(LightAlarmMapper('http://192.168.42.100:8080/'))
+dispatcher.register_mapper_resource(SpeakerAlarmMapper('http://192.168.42.100:8080/'), priority=1) # TODO Change
+dispatcher.start('http://localhost:7894/', 7894)
